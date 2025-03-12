51c2016899db4e3e1f9dcf0a1175e2979ac30be9
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-03-12 14:07:51 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,31 @@
+LINK="https://raw.githubusercontent.com/Sandhj/Wep-panel/main/"
+
+mkdir -p /root/web/templates
+cd web
+wget -q ${LINK}app.py
+
+cd templates
+#Home
+wget -q ${LINK}login.html
+wget -q ${LINK}register.html
+wget -q ${LINK}dash_admin.html
+wget -q ${LINK}dash_guest.html
+
+#Server
+wget -q ${LINK}add_server.html
+wget -q ${LINK}delete_server.html
+wget -q ${LINK}status_server.html
+
+#Users
+wget -q ${LINK}add_balance.html
+wget -q ${LINK}kurangi_saldo.html
+wget -q ${LINK}users.html
+
+#Vpn
+wget -q ${LINK}create_ssh.html
+wget -q ${LINK}create_vmess.html
+wget -q ${LINK}create_vless.html
+wget -q ${LINK}create_trojan.html
+wget -q ${LINK}delete_account.html
+wget -q ${LINK}result.html
+wget -q ${LINK}riwayat.html
